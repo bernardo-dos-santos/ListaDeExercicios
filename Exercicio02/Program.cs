508323706ea948985d24fffbb8bd04bbe8906d22
Bernardo dos Santos Ferreira <bernardosantospg@gmail.com>
2025-03-20 15:24:44 -0300

    Exercicio02

---
+++ Exercicio02/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercicio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float raio, altura;
+            Console.WriteLine("Digite as medidas de um cilindro: ");
+            Console.WriteLine("Raio: ");
+            raio = float.Parse(Console.ReadLine()!);
+            Console.WriteLine("Altura: ");
+            altura = float.Parse(Console.ReadLine()!);
+            double volume = Math.PI * Math.Pow(raio,2) * altura;
+            Console.WriteLine("O volume desse cilindro é" + volume);
+        }
+    }
+}
