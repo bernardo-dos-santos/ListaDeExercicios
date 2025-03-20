e14d6b40bc4e401a058a0adb3056a9d415c41bf5
Bernardo dos Santos Ferreira <bernardosantospg@gmail.com>
2025-03-20 15:37:53 -0300

    Exercicio03

---
+++ Exercicio03/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercicio03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float kmInicial, kmFinal, consumoCombustivel, kmPercorrido, consumoPorLitro;  
+            Console.Write("Digite a quilometragem inicial: ");
+            kmInicial = float.Parse(Console.ReadLine()!);
+            Console.WriteLine();
+            Console.Write("Digite a quilometragem final: ");
+            kmFinal = float.Parse(Console.ReadLine()!);
+            Console.WriteLine();
+            Console.Write("Digite o consumo de combustivel (em litros) ");
+            consumoCombustivel = float.Parse(Console.ReadLine()!);
+            Console.WriteLine();
+
+            kmPercorrido = kmFinal - kmInicial;
+            consumoPorLitro = kmPercorrido / consumoCombustivel;
+
+            Console.WriteLine($"Seu Consumo de combustivel por litro foi de: {consumoPorLitro}");
+
+
+        }
+    }
+}
