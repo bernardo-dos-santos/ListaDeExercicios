cc39973409e0719cfda9e7dd485f68f610694bda
Bernardo dos Santos Ferreira <bernardosantospg@gmail.com>
2025-03-20 15:18:16 -0300

    Exerc01

---
+++ Exercicio01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq.Expressions;
+
+namespace Exercicio01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float comprimento, largura, altura;
+            Console.WriteLine("Digite as medidas de uma caixa retangular: ");
+            Console.WriteLine("Comprimento: ");
+            comprimento = float.Parse(Console.ReadLine()!);
+            Console.WriteLine("Altura: ");
+            altura = float.Parse(Console.ReadLine()!);
+            Console.WriteLine("Largura: ");
+            largura = float.Parse(Console.ReadLine()!);
+            float volume = comprimento * largura * altura;
+            Console.WriteLine("O volume dessa caixa é"+ volume);
+            
+        }
+    }
+}
