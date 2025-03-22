605e7ccfd39c39318dc8414b8af9ce3b3b629593
Bernardo dos Santos Ferreira <bernardosantospg@gmail.com>
2025-03-22 15:46:51 -0300

    Exercicio 04 e 05

---
+++ Exercicio04/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercicio04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float temperaturaC, temperaturaF;
+            Console.Write("Digite a temperatura em Celsius: ");
+            temperaturaC = float.Parse(Console.ReadLine()!);
+            Console.WriteLine();
+            temperaturaF = (temperaturaC * 9 / 5) + 32;
+            Console.WriteLine("Essa temperatura em fahrenheit é: " + temperaturaF);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercicio05/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercicio05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal salarioBase, salarioFinal, percentual, totalVendas, comissao;
+
+            Console.Write("Digite o Salário base do empregado: R$");
+            salarioBase = decimal.Parse(Console.ReadLine()!);
+            Console.WriteLine();
+            Console.Write("Digite o Total de vendas: R$");
+            totalVendas = decimal.Parse(Console.ReadLine()!);
+            Console.WriteLine();
+            Console.Write($"Digite o percentual de comissão: ");
+            percentual = decimal.Parse(Console.ReadLine()!);
+            Console.WriteLine();
+            comissao = totalVendas * percentual / 100;
+            salarioFinal = salarioBase + comissao;
+
+            Console.WriteLine($"O salário total desse empregado é: {salarioFinal}");
+            Console.ReadLine();
+        }
+    }
+}
