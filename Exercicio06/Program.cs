e858820ba7abfb9381d3c7de5e0345f9d6cbd5cd
Bernardo dos Santos Ferreira <bernardosantospg@gmail.com>
2025-03-22 16:16:57 -0300

    Exercicio06

---
+++ Exercicio06/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercicio06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite quantas notas você tem para calcularmos sua média harmônica");
+            int quantasNotas = int.Parse(Console.ReadLine()!);
+            float somaDasDivisoes = 0;
+            for (int contador = 1; contador <= quantasNotas; contador++ )
+            {
+                Console.WriteLine($"Nota {contador}: ");
+                 float nota = float.Parse(Console.ReadLine()!);
+                 somaDasDivisoes += 1 / nota;
+            }
+            float media = quantasNotas / somaDasDivisoes;
+            Console.WriteLine($"Sua média é: {media.ToString("F2")}");
+            Console.ReadLine();
+
+            
+
+        }
+    }
+}
