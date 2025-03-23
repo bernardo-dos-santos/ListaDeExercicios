501c96fb8a37462d175ecb62c47ec66f14bb622e
Bernardo dos Santos Ferreira <bernardosantospg@gmail.com>
2025-03-23 17:38:48 -0300

    Exercicio 19

---
+++ Exercicio19/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercicio19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int numero = 1; numero < 501; numero++)
+            {
+               if(numero%2 != 0 && numero%3 == 0)
+                {
+                    Console.WriteLine(numero);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
