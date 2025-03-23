685f290d6262e197f910ffabf1e3e546d0d3c6a6
Bernardo dos Santos Ferreira <bernardosantospg@gmail.com>
2025-03-23 15:09:52 -0300

    Exercicio 12

---
+++ Exercicio12/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercicio12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o salário do funcionário: R$");
+            decimal salario = decimal.Parse(Console.ReadLine()!);
+            decimal salarioAumento = salario + (salario * 15 / 100);
+            decimal salarioDesconto = salarioAumento * 92 / 100;
+
+            Console.WriteLine($"O salário com aumento é de R${salarioAumento}");
+            Console.WriteLine($"O salário final, com impostos, é de R${salarioDesconto}");
+            Console.ReadLine();
+
+        }
+    }
+}
