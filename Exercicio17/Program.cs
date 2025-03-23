3bb34ec0a7eed7f7ba347d4ada760156e1ba60f1
Bernardo dos Santos Ferreira <bernardosantospg@gmail.com>
2025-03-23 17:28:22 -0300

    Exercicio 17

---
+++ Exercicio17/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercicio17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite dois números: ");
+
+            Console.Write("Numero 1: ");
+            int num1 = int.Parse(Console.ReadLine()!);
+            Console.Write("Numero 2: ");
+            int num2 = int.Parse(Console.ReadLine()!);
+            int resultado;
+            if(num1 == num2)
+            {
+                resultado = num1 + num2;
+                Console.WriteLine("O resultado é: " + resultado);
+            } else
+            {
+                resultado = num1 * num2;
+                Console.WriteLine("O resultado é "+ resultado);      
+            }
+            Console.ReadLine();
+        }
+    }
+}
