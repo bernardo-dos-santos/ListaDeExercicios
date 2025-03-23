0bd3bece1b2f0bf5e28d964b6686a6ed10cae644
Bernardo dos Santos Ferreira <bernardosantospg@gmail.com>
2025-03-23 14:58:10 -0300

    Exercicio 11

---
+++ Exercicio11/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercicio11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o seu nome: ");
+            string nome = Console.ReadLine()!;
+            Console.Write("Digite sua idade: ");
+            int idade = int.Parse(Console.ReadLine()!);
+            int dias = idade * 365;
+            Console.WriteLine($"{nome}, você já viveu {dias} dias");
+            Console.ReadLine();
+        }
+    }
+}
