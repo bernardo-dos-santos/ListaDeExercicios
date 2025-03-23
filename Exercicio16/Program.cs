9d01418ae1c8095f6d1e2ac390aded2ec5975bf3
Bernardo dos Santos Ferreira <bernardosantospg@gmail.com>
2025-03-23 15:52:11 -0300

    Exercicio 16

---
+++ Exercicio16/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercicio16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            int numero = int.Parse(Console.ReadLine()!);
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("Esse número é par");
+            } else
+            {
+                Console.WriteLine("Esse número é ímpar");
+            }
+            Console.ReadLine();
+        }
+    }
+}
