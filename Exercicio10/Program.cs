41b98bfdf95292010d83ba2331fcb0ddc67c8bce
Bernardo dos Santos Ferreira <bernardosantospg@gmail.com>
2025-03-23 14:52:56 -0300

    Exercicio 10

---
+++ Exercicio10/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercicio10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a quantidade de pães: ");
+            int paes = int.Parse(Console.ReadLine()!);
+            Console.Write("Digite a quantidade de broas: ");
+            int broas = int.Parse(Console.ReadLine()!);
+
+            double valorFinal = (paes * 0.12) + (broas * 1.5);
+            double poupanca = valorFinal * 0.1;
+            Console.WriteLine($"O seu valor arrecadado hoje foi de: R${valorFinal.ToString("F2")}");
+            Console.WriteLine($"O valor destinado para a poupança é de R${poupanca.ToString("F2")}");
+            Console.ReadLine();
+        }
+    }
+}
