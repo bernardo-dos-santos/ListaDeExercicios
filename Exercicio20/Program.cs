4ae54fa9040644c508ea6d3a99f98960bf1331b9
Bernardo dos Santos Ferreira <bernardosantospg@gmail.com>
2025-03-23 17:42:48 -0300

    Exercicio 20

---
+++ Exercicio20/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercicio20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            int numero = int.Parse(Console.ReadLine()!);
+            for (int contador = 0; contador < 11; contador++)
+            {
+                Console.WriteLine($"{numero} X {contador} = {numero * contador}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
