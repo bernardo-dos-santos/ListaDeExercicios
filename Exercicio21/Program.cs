2d24d029a4f513d00a56596ab4078b22fafaab1f
Bernardo dos Santos Ferreira <bernardosantospg@gmail.com>
2025-03-23 18:04:43 -0300

    Exercicio 21

---
+++ Exercicio21/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercicio21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número, iremos calcular o fatorial dele(apenas até o numero 31)");
+            int numero = int.Parse(Console.ReadLine()!);
+            int resultado = numero;
+            for (int tamanho = numero - 1; tamanho > 0; tamanho--)
+            {
+                resultado *= tamanho;
+                
+            }
+            Console.WriteLine($"{numero}! = {resultado}");
+            Console.ReadLine();
+        }
+    }
+}
