7fb30e1bfad95168a8f18a86836f0b4db12e9087
Bernardo dos Santos Ferreira <bernardosantospg@gmail.com>
2025-03-23 18:29:00 -0300

    Exercicio 22

---
+++ Exercicio22/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercicio22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos termos da Sequência de Fibonacci você desja? ");
+            int num = int.Parse(Console.ReadLine()!);
+            
+            int primeiro = 0, segundo = 1, seguinte;
+            Console.Write($"{segundo} ");
+
+            for (int i = 1; i < num; i++ )
+            {
+                seguinte = primeiro + segundo;
+                Console.Write($" {seguinte} ");
+                primeiro = segundo;
+                segundo = seguinte;
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
