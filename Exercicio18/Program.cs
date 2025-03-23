981b1927f78f3ae612cd307c52d78493517d1409
Bernardo dos Santos Ferreira <bernardosantospg@gmail.com>
2025-03-23 17:31:48 -0300

    Exercicio 18

---
+++ Exercicio18/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercicio18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int contador = 100; contador < 201; contador++)
+            {
+                if (contador % 2 != 0 )
+                {
+                    Console.WriteLine(contador);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
