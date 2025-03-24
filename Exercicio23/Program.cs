b1ba527e4b16df6a64d9e60ad7178aade312b4fd
Bernardo dos Santos Ferreira <bernardosantospg@gmail.com>
2025-03-23 23:59:44 -0300

    Exercicio 23

---
+++ Exercicio23/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercicio23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           for (int numero = 1; numero < 101; numero++)
+            {
+                if(numero%3 == 0 && numero %5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                } else if (numero%3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                } else if (numero % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(numero);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
